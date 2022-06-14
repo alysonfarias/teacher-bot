33f13c60fa0c3fc582250cee1a6d3748eb5b89f5
Alyson <ramos.alysonfarias@gmail.com>
2022-06-14 20:08:29 -0300

    feat: Initial config, database connection

---
+++ Hackathon.Infrastructure/Context/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Hackathon.Infrastructure.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        // Domain
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+        }
+    }
+}
