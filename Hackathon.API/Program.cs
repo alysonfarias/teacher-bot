dcbf0f70ed0f25ddca331704d9850fcfece9ea2b
Alyson <ramos.alysonfarias@gmail.com>
2022-06-17 17:53:25 -0300

    fix: connection string on user-secret

--- Hackathon.API/Program.cs
+++ Hackathon.API/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("AWS_SQLServerConnection"));
+    options.UseSqlServer(builder.Configuration["ConnectionString:AWS_SQLServerConnection"]);
 });
 
 
