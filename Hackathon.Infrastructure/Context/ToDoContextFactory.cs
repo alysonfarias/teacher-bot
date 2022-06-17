32002c98346d98a0fb2ea3eafba5a69bf74dd133
Alyson <ramos.alysonfarias@gmail.com>
2022-06-17 15:20:32 -0300

    feat: update dependencies

--- Hackathon.Infrastructure/Context/ToDoContextFactory.cs
+++ Hackathon.Infrastructure/Context/ToDoContextFactory.cs
@@ -1,6 +1,15 @@
-﻿namespace Hackathon.Infrastructure.Context
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Hackathon.Infrastructure.Context
 {
-    internal class ToDoContextFactory
+    public class ToDoContextFactory : IDesignTimeDbContextFactory<ApplicationContext>
     {
+        public ApplicationContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<ApplicationContext>();
+            // builder.UseSqlServer(configuration.GetConnectionString("AWS_SQLServerConnection"));
+            return new ApplicationContext(builder.Options);
+        }
     }
 }
