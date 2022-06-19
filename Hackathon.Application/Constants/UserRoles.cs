22f00c43f317532b08d50bd6c3ff0fa96d1c9c29
muriloschiler <muriloschiler.ls@gmail.com>
2022-06-19 09:03:34 -0300

    feat: Creating the UserRoles Const to authentication

---
+++ Hackathon.Application/Constants/UserRoles.cs
@@ -0,0 +1,10 @@
+namespace Hackathon.Application.Constants
+{
+    public class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string Structor = "Structor";
+        public const string Student = "Student";
+        
+    }
+}
