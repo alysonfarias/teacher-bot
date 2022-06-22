a046102f4a81e59372cfece789182c12c4e96c55
Wartelon   Malthus <wartelon.malthus@gmail.com>
2022-06-22 10:51:30 -0300

    -Feat: Criando as validações do AcitivityRequest

---
+++ Hackathon.Application/Validators/ActivityValidator.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using Hackathon.Application.DTOS.Activity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon.Application.Validators
+{
+    public class ActivityValidator : AbstractValidator<ActivityRequest>
+    {
+        public ActivityValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty()
+                .WithMessage("Campo Obrigatorio")
+                .Length(100)
+                .WithMessage("A atividade precisa ter um titulo");
+
+            RuleFor(x => x.Description)
+               .NotEmpty()
+               .WithMessage("Campo Obrigatorio")
+               .Length(250)
+               .WithMessage("A atividade precisa ter um titulo");
+
+            RuleFor(x => x.DueDate)
+                .NotEmpty().WithMessage("Campo Obrigatorio")
+                .LessThan(x => DateTime.Now)
+                .WithMessage("Data invalida! A data do vencimento não pode está no passado");
+        }
+
+    }
+}
